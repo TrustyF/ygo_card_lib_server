656ba4ac5aec065a5e834b6f6111be8187b09568
Trusty <sirjacobs.arthur@gmail.com>
2023-10-07 18:35:48 -0700

    Small changes

--- flask_blueprints/card_blueprint.py
+++ flask_blueprints/card_blueprint.py
@@ -49,7 +49,7 @@ def get_all():
     query = (
         db.session
         .query(UserCard)
-        .filter(UserCard.storage_id.notin_([11, 7, 4, 3, 2, 1]))
+        # .filter(UserCard.storage_id.notin_([11, 7, 4, 3, 2, 1]))
     )
 
     match ordering:
