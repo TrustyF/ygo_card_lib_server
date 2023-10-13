2400a1bc2ef1e25d1b815561f2553cd08cfef54b
Trusty <sirjacobs.arthur@gmail.com>
2023-10-12 18:33:19 -0700

    Attempt to fix login

--- app.py
+++ app.py
@@ -41,14 +41,12 @@ app = Flask(__name__)
 #     databasename="TrustyFox$ygo_cards_library",
 # )
 
-# db_username = os.getenv('MYSQL_DATABASE_USERNAME')
-# db_password = os.getenv('MYSQL_DATABASE_PASSWORD')
-# db_name = 'TrustyFox$ygo_cards_library'
-#
-# app.config["SQLALCHEMY_DATABASE_URI"] = \
-#     f'mysql+pymysql://{db_username}:{db_password}@TrustyFox.mysql.pythonanywhere-services.com/{db_name}'
+db_username = os.getenv('MYSQL_DATABASE_USERNAME')
+db_password = os.getenv('MYSQL_DATABASE_PASSWORD')
+db_name = 'TrustyFox$ygo_cards_library'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://TrustyFox.mysql.pythonanywhere-services.com'
+app.config["SQLALCHEMY_DATABASE_URI"] = \
+    f'mysql+pymysql://{db_username}:{db_password}@TrustyFox.mysql.pythonanywhere-services.com:3306/{db_name}'
 
 CORS(app)
 
