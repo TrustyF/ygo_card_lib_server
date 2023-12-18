f8c29db00f4251740bea25dcbf5bb7111f24bb4d
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2023-12-18 11:42:35 -0800

    Added staple filter

--- flask_blueprints/card_blueprint.py
+++ flask_blueprints/card_blueprint.py
@@ -51,6 +51,8 @@ def get_card():
             query = query.order_by(UserCard.updated_at.desc())
         case 'price':
             query = query.order_by(Card.card_price.desc())
+        case 'staple':
+            query = query.filter(CardTemplate.is_staple == 1).order_by(CARD_TYPE_PRIORITY,CardTemplate.name)
 
     if storage is not None:
         query = query.filter(UserCard.storage_id == storage)
